ba0b95cbad88e03b839e3cff5e48a232a0580eae
Douglas Lovell <doug@wbreeze.com>
2017-09-21 10:39:37 -0300

    Add representation of game board

---
+++ lib/ttt/game_board.rb
@@ -0,0 +1,92 @@
+require 'json'
+
+class GameBoard
+  def initialize
+    @state = Array.new(9)
+    reset
+  end
+
+  def reset
+    @state.fill(:n)
+  end
+
+  def get(row, col)
+    @state[pos(row,col)]
+  end
+
+  def set_o(row,col)
+    @state[pos(row,col)] = :o
+  end
+
+  def set_x(row,col)
+    @state[pos(row,col)] = :x
+  end
+
+  def clear(row,col)
+    @state[pos(row,col)] = :n
+  end
+
+  def is_complete
+    [:draw, :win_x, :win_o].include? state
+  end
+
+  def state
+    if has_win_x
+      :win_x
+    elsif has_win_o
+      :win_o
+    elsif is_full
+      :draw
+    else
+      :incomplete
+    end
+  end
+
+  def to_json
+    pretty = @state.map do |p|
+      case p
+      when :x
+        'x'
+      when :o
+        'o'
+      else
+        ' '
+      end
+    end
+    JSON.fast_generate({ game_state: pretty })
+  end
+
+  ###
+  private
+  ###
+
+  def pos(row, col)
+    row * 3 + col
+  end
+
+  def has_win(xo)
+    wp = [[0,3,6],[1,4,7],[2,5,8],
+     [0,1,2],[3,4,5],[6,7,8],
+     [0,4,8],[2,4,6]
+    ].find do |line|
+      line.reduce(true) do |m, p|
+        m && (@state[p] == xo)
+      end
+    end
+    !wp.nil?
+  end
+
+  def has_win_x
+    has_win(:x)
+  end
+
+  def has_win_o
+    has_win(:o)
+  end
+
+  def is_full
+    @state.inject(true) do |m, v|
+      m && (v != :n)
+    end
+  end
+end
---
+++ spec/game_board_spec.rb
@@ -0,0 +1,218 @@
+require 'json'
+
+RSpec.describe 'GameBoard' do
+  before :all do
+    @range = (0..2)
+  end
+
+  before :each do
+    @gb = GameBoard.new
+  end
+
+  it 'records x at a row column' do
+    for row in @range
+      for col in @range
+        expect(@gb.get(row, col)).to eq :n
+        @gb.set_x(row, col)
+        expect(@gb.get(row, col)).to eq :x
+      end
+    end
+  end
+
+  it 'records o at a row column' do
+    for row in @range
+      for col in @range
+        expect(@gb.get(row, col)).to eq :n
+        @gb.set_o(row, col)
+        expect(@gb.get(row, col)).to eq :o
+      end
+    end
+  end
+
+  it 'enables clear at a row column' do
+    for row in @range
+      for col in @range
+        expect(@gb.get(row, col)).to eq :n
+        @gb.set_o(row, col)
+        expect(@gb.get(row, col)).to eq :o
+        @gb.clear(row, col)
+        expect(@gb.get(row, col)).to eq :n
+      end
+    end
+  end
+
+  it 'enables reset' do
+    for row in @range
+      for col in @range
+        expect(@gb.get(row, col)).to eq :n
+        @gb.set_x(row, col)
+        expect(@gb.get(row, col)).to eq :x
+      end
+    end
+    @gb.reset
+    for row in @range
+      for col in @range
+        expect(@gb.get(row, col)).to eq :n
+      end
+    end
+  end
+
+  it 'allows overwrite' do
+    for row in @range
+      for col in @range
+        expect(@gb.get(row, col)).to eq :n
+        @gb.set_x(row, col)
+        expect(@gb.get(row, col)).to eq :x
+        @gb.set_o(row, col)
+        expect(@gb.get(row, col)).to eq :o
+        @gb.set_x(row, col)
+        expect(@gb.get(row, col)).to eq :x
+      end
+    end
+  end
+
+  it 'recognizes in play' do
+    @gb.set_x(0,1)
+    @gb.set_o(1,1)
+    expect(@gb.state).to eq :incomplete
+    expect(@gb.is_complete).to be false
+  end
+
+  it 'recognizes a draw' do
+    [[0,0],[0,2],[1,0],[1,2],[2,1]].each do |p|
+      @gb.set_x(p[0],p[1])
+    end
+    [[0,1],[1,1],[2,0],[2,2]].each do |p|
+      @gb.set_o(p[0],p[1])
+    end
+    expect(@gb.state).to eq :draw
+    expect(@gb.is_complete).to be true
+  end
+
+  it 'recognizes success of x' do
+    [[0,0],[0,1],[1,0],[2,0]].each do |p|
+      @gb.set_x(p[0],p[1])
+    end
+    [[0,2],[1,1],[1,2]].each do |p|
+      @gb.set_o(p[0],p[1])
+    end
+    expect(@gb.state).to eq :win_x
+    expect(@gb.is_complete).to be true
+  end
+
+  it 'recognizes success of o' do
+    [[0,0],[0,1],[1,1]].each do |p|
+      @gb.set_x(p[0],p[1])
+    end
+    [[0,2],[1,2],[2,2]].each do |p|
+      @gb.set_o(p[0],p[1])
+    end
+    expect(@gb.state).to eq :win_o
+    expect(@gb.is_complete).to be true
+  end
+
+  it 'recognizes column one win' do
+    [[0,0],[1,0],[2,0]].each do |p|
+      @gb.set_x(p[0],p[1])
+    end
+    [[0,1],[0,2]].each do |p|
+      @gb.set_o(p[0],p[1])
+    end
+    expect(@gb.state).to eq :win_x
+    expect(@gb.is_complete).to be true
+  end
+
+  it 'recognizes column two win' do
+    [[0,1],[1,1],[2,1]].each do |p|
+      @gb.set_x(p[0],p[1])
+    end
+    [[0,0],[0,2]].each do |p|
+      @gb.set_o(p[0],p[1])
+    end
+    expect(@gb.state).to eq :win_x
+    expect(@gb.is_complete).to be true
+  end
+
+  it 'recognizes column three win' do
+    [[0,2],[1,2],[2,2]].each do |p|
+      @gb.set_x(p[0],p[1])
+    end
+    [[0,0],[0,1]].each do |p|
+      @gb.set_o(p[0],p[1])
+    end
+    expect(@gb.state).to eq :win_x
+    expect(@gb.is_complete).to be true
+  end
+
+  it 'recognizes row one win' do
+    [[0,0],[0,1],[0,2]].each do |p|
+      @gb.set_x(p[0],p[1])
+    end
+    [[1,0],[2,0]].each do |p|
+      @gb.set_o(p[0],p[1])
+    end
+    expect(@gb.state).to eq :win_x
+    expect(@gb.is_complete).to be true
+  end
+
+  it 'recognizes row two win' do
+    [[1,0],[1,1],[1,2]].each do |p|
+      @gb.set_x(p[0],p[1])
+    end
+    [[0,0],[2,0]].each do |p|
+      @gb.set_o(p[0],p[1])
+    end
+    expect(@gb.state).to eq :win_x
+    expect(@gb.is_complete).to be true
+  end
+
+  it 'recognizes row three win' do
+    [[2,0],[2,1],[2,2]].each do |p|
+      @gb.set_x(p[0],p[1])
+    end
+    [[0,0],[1,0]].each do |p|
+      @gb.set_o(p[0],p[1])
+    end
+    expect(@gb.state).to eq :win_x
+    expect(@gb.is_complete).to be true
+  end
+
+  it 'recognizes backward diagonal win' do
+    [[0,0],[1,1],[2,2]].each do |p|
+      @gb.set_x(p[0],p[1])
+    end
+    [[0,1],[1,0]].each do |p|
+      @gb.set_o(p[0],p[1])
+    end
+    expect(@gb.state).to eq :win_x
+    expect(@gb.is_complete).to be true
+  end
+
+  it 'recognizes forward diagonal win' do
+    [[0,2],[1,1],[2,0]].each do |p|
+      @gb.set_x(p[0],p[1])
+    end
+    [[0,1],[1,0]].each do |p|
+      @gb.set_o(p[0],p[1])
+    end
+    expect(@gb.state).to eq :win_x
+    expect(@gb.is_complete).to be true
+  end
+
+  it 'provides a compact representation' do
+    [[0,2],[1,1],[2,0]].each do |p|
+      @gb.set_x(p[0],p[1])
+    end
+    [[0,1],[1,0]].each do |p|
+      @gb.set_o(p[0],p[1])
+    end
+    jss = @gb.to_json
+    expect(jss).to be_a_kind_of(String)
+    gbs = JSON.parse(jss)
+    expect(gbs).to be_a_kind_of(Hash)
+    expect(gbs['game_state']).to be_a_kind_of(Array)
+    expect(gbs['game_state']).to match_array(
+      [ ' ', 'o', 'x', 'o', 'x', ' ', 'x', ' ', ' ' ]
+    )
+  end
+end
